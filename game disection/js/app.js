ca0e01773f10fe45fd64341fb6a0c8eba6192014
PsychoDoge <57685858+PsychoDoge@users.noreply.github.com>
2020-10-02 19:37:18 -0400

    Add files via upload

---
+++ game disection/js/app.js
@@ -0,0 +1,9 @@
+var bodyText = document.getElementById("Layer_1");
+
+console.log(bodyText)
+
+function changeElement(){
+
+bodyText.innerHTML = '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 760.36 757"><defs><style>.cls-1{fill:red;}</style></defs><rect class="cls-1" x="207.35" y="0.5" width="32" height="756"/><path d="M807,128V883H776V128h31m1-1H775V884h33V127Z" transform="translate(-568.15 -127)"/><rect class="cls-1" x="545.35" y="0.5" width="32" height="756"/><path d="M1145,128V883h-31V128h31m1-1h-33V884h33V127Z" transform="translate(-568.15 -127)"/><rect class="cls-1" x="930.79" y="296.99" width="32" height="756" transform="translate(-305 1488.56) rotate(-89.48)"/><path d="M569.45,656l755,6.91-.28,31-755-6.9.29-31m-1-1-.3,33,757,6.92.3-33-757-6.92Z" transform="translate(-568.15 -127)"/><rect class="cls-1" x="933.88" y="-40.99" width="32" height="756" transform="translate(36.04 1156.76) rotate(-89.48)"/><path d="M572.54,318.06l755,6.9-.28,31-755-6.91.29-31m-1-1-.31,33,757,6.92.3-33-757-6.92Z" transform="translate(-568.15 -127)"/></svg>'
+}
+console.log(changeElement);
