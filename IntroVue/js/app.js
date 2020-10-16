297331a95315d31553c64d5c063a4c25b2c478c9
PsychoDoge <57685858+PsychoDoge@users.noreply.github.com>
2020-10-16 14:14:00 -0400

    Add files via upload

---
+++ IntroVue/js/app.js
@@ -0,0 +1,53 @@
+var bookOne = new Vue({
+el:'#bookOne',
+data:{
+button: 'Hide book One',
+message:'the bluecable',
+message1: 'Jon Joe',
+message2: '🤡',
+seen: true,
+},
+})
+var bookOne = new Vue({
+el:'#bookTwo',
+data:{
+button: 'Hide book Two',
+message:'Saving the Turtles A biography',
+message1: 'Elexis Aristole',
+message2: '🐢',
+seen: true,
+},
+})
+
+
+
+
+
+
+/* old code Might need
+var title = new Vue({
+el:'#title',
+data:{message:'the bluecable',
+seen: true,
+},
+})
+var author = new Vue({
+el:'#author',
+data:{message1:'joe jon'}
+})
+var covor = new Vue({
+el:'#cover',
+data:{message2:'>:D'}
+})
+var title1 = new Vue({
+el:'#title2',
+data:{message:'Babble the third a biography'}
+})
+var author1 = new Vue({
+el:'#author2',
+data:{message1:'Mary Blue'}
+})
+var covor1 = new Vue({
+el:'#cover2',
+data:{message2:'<:O'}
+})*/
